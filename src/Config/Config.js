1cadcf845509413a48ddaedb0322c2014fc4eb1b
Vinicius-Jardim <8230737@estg.ipp.pt>
2025-01-03 21:24:00 +0000

    ok

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,8 +1,7 @@
 const config = {
-    API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
-    WS_URL: process.env.REACT_APP_WS_URL || 'ws://localhost:5000/ws',
+    API_URL: process.env.REACT_APP_API_URL || 'https://dre-backend-e5etfrc8dranhfd3.spaincentral-01.azurewebsites.net',
+    WS_URL: process.env.REACT_APP_WS_URL || 'wss://dre-backend-e5etfrc8dranhfd3.spaincentral-01.azurewebsites.net/ws',
     JWT_SECRET: process.env.REACT_APP_JWT_SECRET || 'your-secret-key'
 };
-
 
 export default config;
